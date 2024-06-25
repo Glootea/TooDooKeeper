9cb4565759ce76aedbcf351b14dabc452a774770
Glootea <sasharyba17@gmail.com>
2024-06-25 14:09:03 +0300

    fix: url fullness (by changing provider)

--- lib/common/data/data_sources/url_shortener.dart
+++ lib/common/data/data_sources/url_shortener.dart
@@ -5,12 +5,13 @@ class UrlShortener {
   static Future<String> shortenUrl(String url) async {
     try {
       final dio = Dio();
-      final response = await dio.get<Map<String, dynamic>>(
-        'https://ulvis.net/API/write/get',
+      final response = await dio.post<Map<String, dynamic>>(
+        'https://spoo.me',
         queryParameters: {'url': url},
+        options: Options(headers: {'Accept': 'application/json', 'Content-Type': 'application/x-www-form-urlencoded'}),
       );
       // ignore: avoid_dynamic_calls
-      final short = response.data?['data']['url'] as String;
+      final short = response.data?['short_url'] as String;
       return short;
     } catch (e) {
       logger.e('Failed to shorten url: $url, error: $e');
