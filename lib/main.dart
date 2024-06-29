1816f3285a832bc2c34dfd399dd5d29d02e00e17
Glootea <rybkinsasha1@gmail.com>
2024-06-30 02:27:32 +0300

    fix: broken merge

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'dart:async';
 import 'package:flutter/foundation.dart';
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -173,7 +171,6 @@ class InitScreen extends StatelessWidget {
       ),
     );
   }
-
 }
 
 final Logger logger = Logger();
