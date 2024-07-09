496e01f71457a24aaa78f8f2140d38c82e45e9a9
Glootea <rybkinsasha1@gmail.com>
2024-07-09 23:08:18 +0300

    feat: basic todo_list_bloc tests

--- test/mocks/mock_remote_provider.dart
+++ test/mocks/mock_remote_provider.dart
@@ -1,20 +1,10 @@
-import 'package:yandex_summer_school/core/data/data_sources/auth/auth_method_abst.dart';
+import 'package:mocktail/mocktail.dart';
 import 'package:yandex_summer_school/core/data/data_sources/online_database/online_database_abst.dart';
 import 'package:yandex_summer_school/core/data/providers/online/remote_provider_abst.dart';
 
-class MockRemoteProvider implements RemoteProvider {
+class MockRemoteProvider extends Mock implements RemoteProvider {
   MockRemoteProvider(this.database);
 
   @override
   final RemoteDatabase database;
-
-  @override
-  // TODO: implement auth
-  AuthMethod get auth => throw UnimplementedError();
-
-  @override
-  Future<void> logout() {
-    // TODO: implement logout
-    throw UnimplementedError();
-  }
 }
--- test/screens/todo_list_bloc_test.dart
+++ test/screens/todo_list_bloc_test.dart
@@ -14,28 +14,69 @@ void main() async {
   ToDoListBloc? bloc;
   MockRemoteProvider? remote;
 
-  group('TodoListBloc', () {
-    setUp(() {
-      WidgetsFlutterBinding.ensureInitialized();
+  group('TodoListBloc:', () {
+    group('Internet is connected:', () {
+      // Set up fresh copy of bloc
+      setUp(() {
+        WidgetsFlutterBinding.ensureInitialized();
 
-      database = MockRemoteDatabase();
-      remote = MockRemoteProvider(database!);
-      var local = FakeLocalDatabase();
-      when(remote!.database.getToDoList).thenAnswer((_) async => <ToDo>[]);
-      when(() => remote!.database.updateToDoList(any<List<ToDo>>())).thenAnswer((_) async => <ToDo>[]);
-      var toDoProvider = ToDoProvider(
-        localDatabase: local,
-        onlineProvider: remote!,
-      );
-      local = FakeLocalDatabase();
-      toDoProvider = ToDoProvider(
-        localDatabase: local,
-        onlineProvider: remote!,
-      );
-      bloc = ToDoListBloc(toDoProvider);
+        database = MockRemoteDatabase();
+        remote = MockRemoteProvider(database!);
+        final local = FakeLocalDatabase();
+
+        // Provide default implementations for initialization
+        when(remote!.database.getToDoList).thenAnswer((_) async => <ToDo>[]);
+        when(() => remote!.database.updateToDoList(any<List<ToDo>>())).thenAnswer((_) async => <ToDo>[]);
+
+        final toDoProvider = ToDoProvider(localDatabase: local, onlineProvider: remote!);
+        bloc = ToDoListBloc(toDoProvider);
+      });
+      test('Initial state is correct', () {
+        expect(bloc!.state, const LoadingState());
+      });
+
+      test('Get main state from online', () async {
+        final state = await bloc?.stream.first;
+        expect(state, const TypeMatcher<MainState>());
+        if (state is MainState) {
+          expect(state.todos, hasLength(0));
+          expect(state.networkConnectionPresent, true);
+          expect(state.query, const ToDoListQuery());
+          expect(state.showDone, true);
+        } else {
+          throw TestFailure('Unexpected state: $state');
+        }
+      });
     });
-    test('Initial state is correct', () {
-      expect(bloc!.state, const LoadingState());
+
+    group('Internet not connected:', () {
+      setUp(() {
+        WidgetsFlutterBinding.ensureInitialized();
+
+        database = MockRemoteDatabase();
+        remote = MockRemoteProvider(database!);
+        final local = FakeLocalDatabase();
+
+        // Provide default implementations for initialization
+        when(remote!.database.getToDoList).thenAnswer((_) async => null);
+        when(() => remote!.database.updateToDoList(any<List<ToDo>>())).thenAnswer((_) async => null);
+
+        final toDoProvider = ToDoProvider(localDatabase: local, onlineProvider: remote!);
+        bloc = ToDoListBloc(toDoProvider);
+      });
+
+      test('Get main state from local', () async {
+        final state = await bloc?.stream.first;
+        expect(state, const TypeMatcher<MainState>());
+        if (state is MainState) {
+          expect(state.todos, hasLength(0));
+          expect(state.networkConnectionPresent, false);
+          expect(state.query, const ToDoListQuery());
+          expect(state.showDone, true);
+        } else {
+          throw TestFailure('Unexpected state: $state');
+        }
+      });
     });
   });
 }
