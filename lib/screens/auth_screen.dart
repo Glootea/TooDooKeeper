eee2b0c9bfdde7fe3ea8cfa92e642e8c15fd6fd7
Glootea <sasharyba17@gmail.com>
2024-07-17 18:12:20 +0300

    fix: ci, auth screen wide layout

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -13,48 +13,50 @@ class AuthScreen extends StatelessWidget {
     final onlineProvider = context.read<OnlineProvider>();
     final todoTheme = context.watch<ThemeBloc>();
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Image.asset('assets/dino.png', width: 300),
-          const SizedBox(height: 16),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: Text(
-              context.loc.loginNeeded,
-              style: todoTheme.state.textTheme.body,
-              textAlign: TextAlign.center,
-            ),
-          ),
-          const SizedBox(height: 16),
-          OutlinedButton(
-            onPressed: () async {
-              final result = await onlineProvider.login();
-              if (result == true) {
-                if (context.mounted) {
-                  context.pushReplacement('/');
-                }
-              } else {
-                if (context.mounted) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
-                      content: Text(
-                        context.loc.loginFailed,
-                        style: todoTheme.state.textTheme.body,
-                      ),
-                    ),
-                  );
-                }
-              }
-            },
-            child: Text(
-              context.loc.login,
-              style: todoTheme.state.textTheme.button.copyWith(
-                color: todoTheme.state.definedColors.blue,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('assets/dino.png', width: 300),
+            const SizedBox(height: 16),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Text(
+                context.loc.loginNeeded,
+                style: todoTheme.state.textTheme.body,
+                textAlign: TextAlign.center,
               ),
             ),
-          ),
-        ],
+            const SizedBox(height: 16),
+            OutlinedButton(
+              onPressed: () async {
+                final result = await onlineProvider.login();
+                if (result == true) {
+                  if (context.mounted) {
+                    context.pushReplacement('/');
+                  }
+                } else {
+                  if (context.mounted) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text(
+                          context.loc.loginFailed,
+                          style: todoTheme.state.textTheme.body,
+                        ),
+                      ),
+                    );
+                  }
+                }
+              },
+              child: Text(
+                context.loc.login,
+                style: todoTheme.state.textTheme.button.copyWith(
+                  color: todoTheme.state.definedColors.blue,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
